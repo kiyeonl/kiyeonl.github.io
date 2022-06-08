ce8d95b1788bf8cfd4044adfc118d353b83ee9b2
kiyeonl <107113589+kiyeonl@users.noreply.github.com>
2022-06-08 21:53:10 +0900

    Add files via upload

---
+++ trn01/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+////////////////////////////////////////////////////////////////////////////////
+// $('.xi-bars').on('click',function(){
+//     $(this).hide();
+
+// });
+
+// document.querySelector('.xi-bars').addEventListener('click',function(){}); 순수 자바스크립트
+/* document.querySelector('.xi-bars').addEventListener('click',() => {  리액트에서 많이 사용하는 공식 (this를 받지 못함)
+
+}); */
+
+//////////////////////////////////////////////////////////////////////////////
+});
